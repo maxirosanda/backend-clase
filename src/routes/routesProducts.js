c21d41ea5121c231427fc7b663019a747baf64d1
maxirosanda <maxi_rosanda@hotmail.com>
2021-11-02 16:12:07 -0300

    njgbsg

--- src/routes/routesProducts.js
+++ src/routes/routesProducts.js
@@ -6,7 +6,7 @@ const routesProducts = (app) => {
     app.get('/products',controllerProducts.view)
     app.post('/products',controllerProducts.create)
     app.delete('/products',controllerProducts.del)
-    app.put('products',controllerProducts.update)
+    app.put('/products',controllerProducts.update)
 
 }
 
